c8d268853dcdd9282d8768187c56388353c0a9ec
aayushsaini <aayush.r98@gmail.com>
2021-09-12 03:19:36 +0530

    Chat bubble fix

--- src/Components/MessageBar/ChatScreen/ChatBody/ChatBubbles.jsx
+++ src/Components/MessageBar/ChatScreen/ChatBody/ChatBubbles.jsx
@@ -11,7 +11,7 @@ const ChatBubbles = (props) => {
     const body = props.body;
 
     return (
-        <Flex zIndex={-1000} mb={3}>
+        <Flex zIndex={-1000} mb={2}>
             {type==="recieved"?
             <>
             <Box 
