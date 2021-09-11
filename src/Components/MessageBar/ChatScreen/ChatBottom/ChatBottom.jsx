7033fd5fd221bf798d81dd4447f7c728d115a8cb
aayushsaini <aayush.r98@gmail.com>
2021-09-12 03:01:18 +0530

    removed unused components

--- src/Components/MessageBar/ChatScreen/ChatBottom/ChatBottom.jsx
+++ src/Components/MessageBar/ChatScreen/ChatBottom/ChatBottom.jsx
@@ -1,11 +1,10 @@
-import { Box, Button, Flex, Icon, Image, Input, useColorModeValue } from '@chakra-ui/react'
+import { Box, Button, Flex, Image, Input, useColorModeValue } from '@chakra-ui/react'
 import React from 'react'
 import emojiFace from "../../../../Assets/Images/ChatEmoji@2x.png"
 
 const ChatBottom = () => {
 
     const inputBackground = useColorModeValue("gray.200","#0D0D0D");
-    const iconBackground = useColorModeValue("gray.500","gray.500");
 
     return (
         <Box minW="40vw" maxW="40vw" px={4} fontFamily="Poppins">
